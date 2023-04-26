d4acd510eeb8ccda54b89031c8a0d4df513af8ca
Moacir <89947549+moacirescorcio@users.noreply.github.com>
2023-04-26 11:21:46 -0300

    Update piscina.js

--- piscina.js
+++ piscina.js
@@ -11,6 +11,7 @@ function main(){
 
     const volume = (largura*comprimento*profundidade)/1000
     const litros_recomendado = volume * 0.85
+    const arredondar_litros_recomendados = Math.ceil(litros_recomendado / 1000) * 1000
     console.log(`>> Capacidade máxima da psicina é de ${volume} litros`)
     console.log(`>> É recomendado encher a psicina em ${litros_recomendado} litros`)
     console.log('\n')
@@ -18,13 +19,32 @@ function main(){
     const valor = Number(question('Qual o valor a pagar por cada 1000 litros? '))
     let contador = 0
     let total_a_pagar = 0
-    while(contador < litros_recomendado){
+    while(contador < arredondar_litros_recomendados){
         total_a_pagar = total_a_pagar + valor
         contador = contador + 1000
         
     }
 
     console.log(`Total a pagar seria de R$${total_a_pagar.toFixed(2)}`)
+
+    //repor os 10%
+    const perda_de_agua = litros_recomendado * 0.1
+    let valor_pagar_reposicao = 0
+    if (perda_de_agua <= 1000){
+        valor_pagar_reposicao = valor  
+        console.log(`O total a pagar com a reposição de água mensalmente é de R$${valor_pagar_reposicao.toFixed(2)}`) 
+    }else if(perda_de_agua > 1000 ){
+        const arredondar_perda_agua = Math.ceil(perda_de_agua / 1000) * 1000
+        let contador = 0
+        let total_a_pagar_reposicao = 0
+        while(contador < arredondar_perda_agua){
+            total_a_pagar_reposicao = total_a_pagar_reposicao + valor
+            contador = contador + 1000
+            
+        }
+        console.log(`O total a pagar com a reposição de água mensalmente é de R$${total_a_pagar_reposicao.toFixed(2)}`)
+
+    }
 }
 
 main()
