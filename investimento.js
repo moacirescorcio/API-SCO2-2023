911d07874f9ad0d2e8fece612c9ba0bb28668843
Moacir <89947549+moacirescorcio@users.noreply.github.com>
2023-04-26 11:55:57 -0300

    Update investimento.js

--- investimento.js
+++ investimento.js
@@ -1,2 +1,12 @@
 import { question } from "readline-sync";
 
+function main(){
+    const objetivo = question('Qual o seu objetivo? ')
+    const valor_necessário = question('Qual o valor necessário para realizar esse objetivo? ')
+
+    const salario = question('Qual o valor do seu salário? ')
+    const investimento_mensal = question('Quantos % pretende investir por mês? ')
+    const taxa_de_juros = question('Qual da taxa de juros escolhida?(0,01 até 1,00) ')
+}
+
+main()
