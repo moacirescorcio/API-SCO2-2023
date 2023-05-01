5b446b372261b1604b6351f24009d6be8554c1b6
Moacir <89947549+moacirescorcio@users.noreply.github.com>
2023-05-01 09:31:33 -0300

    Create serasa.js

---
+++ serasa.js
@@ -0,0 +1,13 @@
+import { question } from "readline-sync";
+
+// caso não esteja negativado, apresentando um valor entre 0 e 1000. 
+//entrada a,b e c
+//saída pontuação no score 1.0 e 2.0 junto com a categoria
+
+function main(){
+    const a = Number(question('Porcentagem no critério A: '))
+    const b = Number(question('Porcentagem no critério B: '))
+    const c = Number(question('Porcentagem no critério C: '))
+}
+
+main()
